821ad01dab84b8ec8ad864a4318a589e42763af9
Pierre Schweitzer <pierre@reactos.org>
2015-09-05 23:11:00 +0200

    Add an example simulation that computes PI, with its executable to interpret raw results coming from Monte Carlo computation

---
+++ examples/Pi/pi.c
@@ -0,0 +1,78 @@
+/*
+ * COPYRIGHT:        See LICENSE in the top level directory
+ * PROJECT:          PI simulation
+ * FILE:             examples/Pi/pi.c
+ * PURPOSE:          Main simulation file
+ * PROGRAMMER:       Pierre Schweitzer (pierre@reactos.org)
+ */
+
+#include <stdlib.h>
+#include "simulation.h"
+
+int SimulationInit(unsigned int nThreads, unsigned long nEvents, unsigned long fistEvent, void ** context)
+{
+    /* Nothing to do */
+    return 0;
+}
+
+int RunInit(void * context)
+{
+    /* Nothing to do */
+    return 0;
+}
+
+int EventInit(void * context, void * rand, void ** eventContext)
+{
+    /* Only save the rand pointer */
+    *eventContext = rand;
+
+    return 0;
+}
+
+void EventRun(void * context, void * eventContext)
+{
+    double total, inside;
+    TResult result;
+
+    /* We'll compute 10,000 values on each thread */
+    for (total = 0, inside = 0; total < 10000; ++total)
+    {
+        double x = RandU01(eventContext);
+        double y = RandU01(eventContext);
+
+        if (x * x + y * y < 1.0)
+        {
+            ++inside;
+        } 
+    }
+
+    /* We'll return 2 unsigned long */
+    result.fResultLength = 2 * sizeof(double);
+
+    /* First, total and then inside */
+    ((double *)result.fResult)[0] = total;
+    ((double *)result.fResult)[1] = inside;
+
+    /* Write the result */
+    QueueResult(&result, eventContext);
+
+    return;
+}
+
+void EventClear(void * context, void * eventContext)
+{
+    /* Nothing to do */
+    return;
+}
+
+void RunClear(void * context)
+{
+    /* Nothing to do */
+    return;
+}
+
+void SimulationUnload(void * context)
+{
+    /* Nothing to do */
+    return;
+}
---
+++ examples/Pi/result.c
@@ -0,0 +1,73 @@
+/*
+ * COPYRIGHT:        See LICENSE in the top level directory
+ * PROJECT:          PI simulation
+ * FILE:             examples/Pi/pi.c
+ * PURPOSE:          Read results and print Pi
+ * PROGRAMMER:       Pierre Schweitzer (pierre@reactos.org)
+ */
+
+#include "simulation.h"
+#include <string.h>
+#include <stdio.h>    
+
+#define PATH_MAX 0x1000
+
+#define DEFAULT_NAME {'H', 'P', 'C', 's', 'i', 'm', '.', 'o', 'u', 't', '\0'}
+
+int main(int argc, char *argv[])
+{
+    FILE * inFD;
+    char resFile[PATH_MAX] = DEFAULT_NAME;
+    double inside = 0.0, total = 0.0;
+    unsigned short len;
+    double res[2];
+
+    if (argc > 1)
+    {
+        strncpy(resFile, argv[1], PATH_MAX - 1);
+        resFile[PATH_MAX - 1] = '\0';
+    }
+
+    inFD = fopen(resFile, "r");
+    if (inFD == NULL)
+    {
+        fprintf(stderr, "Error while opening %s\n", resFile);
+        return -1;
+    }
+
+    do
+    {
+        /* Skip ID */
+        if (fseek(inFD, SHA384_DIGEST_LENGTH, SEEK_CUR) == -1)
+        {
+            break;
+        }
+
+        /* Sanity check: verify we have correct length */
+        if (fread(&len, sizeof(unsigned short), 1, inFD) != 1)
+        {
+            break;
+        }
+        if (len != 2 * sizeof(double))
+        {
+            break;
+        }
+
+        /* Read the two doubles */
+        if (fread(res, sizeof(double), 2, inFD) != 2)
+        {
+            break;
+        }
+
+        /* Adjust counts */
+        total += res[0];
+        inside += res[1];
+    }
+    while (!feof(inFD));
+    fclose(inFD);
+
+    /* Compute PI for real */
+    printf("Pi: %lf (with %lf samples)\n", (4.0 * inside) / total, total);
+
+    return 0;
+}
